d483d3cefb69c4590b1db05919aada0b685ae767
Kitty <kathryn.harris@ucdenver.edu>
2021-02-23 13:03:50 -0700

    Add baseline for SCI-Compiler project. Selected from examples - nothing unique yet

---
+++ Debug/MCAHP_test_lib.c.o.d
@@ -0,0 +1,12 @@
+Debug/MCAHP_test_lib.c.o: src/MCAHP_test_lib.c src/R5560_SDKLib.h \
+ src/Def.h src/RegisterFile.h src/circular_buffer.h src/MCAHP_test_lib.h
+
+src/R5560_SDKLib.h:
+
+src/Def.h:
+
+src/RegisterFile.h:
+
+src/circular_buffer.h:
+
+src/MCAHP_test_lib.h:
