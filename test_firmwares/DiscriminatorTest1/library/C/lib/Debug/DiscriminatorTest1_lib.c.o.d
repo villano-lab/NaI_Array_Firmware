47b34f7282d5f57846c28a4d9b4f02093f3d1a4a
nuclearGoblin <kathryn.harris@ucdenver.edu>
2022-03-16 16:05:28 -0600

    Create DiscriminatorTest1 example executable and associated files.

---
+++ test_firmwares/DiscriminatorTest1/library/C/lib/Debug/DiscriminatorTest1_lib.c.o.d
@@ -0,0 +1,13 @@
+Debug/DiscriminatorTest1_lib.c.o: src/DiscriminatorTest1_lib.c \
+ src/R5560_SDKLib.h src/Def.h src/RegisterFile.h src/circular_buffer.h \
+ src/DiscriminatorTest1_lib.h
+
+src/R5560_SDKLib.h:
+
+src/Def.h:
+
+src/RegisterFile.h:
+
+src/circular_buffer.h:
+
+src/DiscriminatorTest1_lib.h:
