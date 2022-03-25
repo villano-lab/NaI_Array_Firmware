accd031108897d33e1361392ecac122f55d31e0c
cdms <cmds@example.com>
2022-03-25 16:37:16 -0500

    Create firmware for integration.

---
+++ test_firmwares/Integration/library/C/lib/Debug/Integration_lib.c.o.d
@@ -0,0 +1,12 @@
+Debug/Integration_lib.c.o: src/Integration_lib.c src/R5560_SDKLib.h \
+ src/Def.h src/RegisterFile.h src/circular_buffer.h src/Integration_lib.h
+
+src/R5560_SDKLib.h:
+
+src/Def.h:
+
+src/RegisterFile.h:
+
+src/circular_buffer.h:
+
+src/Integration_lib.h:
---
+++ test_firmwares/Integration/library/Python/plot.py
@@ -0,0 +1,21 @@
+#Imports
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
+import numpy as np
+
+#Settings
+matplotlib.use('tkagg') #Choose backend that can be x-forwarded to windows
+
+#Get and arrange data
+sourcedata = pd.read_csv("../../data/source.csv",header=None,usecols=[0]).to_numpy().flatten()
+backgrounddata = pd.read_csv("../../data/out.csv",header=None,usecols=[0]).to_numpy().flatten()
+sourcedata = np.subtract(sourcedata,200*8192) 	#subtract the baseline (8192) times the gate value.
+backgrounddata = np.subtract(backgrounddata,200*8192)
+bins = np.arange(min(backgrounddata.min(),sourcedata.min()),max(backgrounddata.max(),sourcedata.max()))#Data are bin numbers, so distance <1 will cause gaps.
+#print(data)
+
+#plot
+plt.hist(sourcedata,bins=bins)
+plt.hist(backgrounddata,bins=bins)
+plt.show()
