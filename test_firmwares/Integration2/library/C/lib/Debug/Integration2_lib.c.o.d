559cbba7f55e105a391c0e05ff8970e4a45c0cbf
cdms <cmds@example.com>
2022-03-28 17:02:26 -0500

    sci-compiler file was corrupted so started a new library.

---
+++ test_firmwares/Integration2/library/C/lib/Debug/Integration2_lib.c.o.d
@@ -0,0 +1,13 @@
+Debug/Integration2_lib.c.o: src/Integration2_lib.c src/R5560_SDKLib.h \
+ src/Def.h src/RegisterFile.h src/circular_buffer.h \
+ src/Integration2_lib.h
+
+src/R5560_SDKLib.h:
+
+src/Def.h:
+
+src/RegisterFile.h:
+
+src/circular_buffer.h:
+
+src/Integration2_lib.h:
---
+++ test_firmwares/Integration2/library/C/lib/src/RegisterFile.h
@@ -0,0 +1,9 @@
+#define SCI_REG_Delay 0x00000000
+#define SCI_REG_Cutoff 0x00000001
+#define SCI_REG_Gate 0x00000002
+#define SCI_REG_Reset 0x00000003
+#define SCI_REG_Integral 0x00000004
+#define SCI_REG_Counts 0x00000005
+#define SCI_REG_Read 0x00000006
+#define SCI_REG_Empty 0x00000007
+#define SCI_REG_Full 0x00000008
