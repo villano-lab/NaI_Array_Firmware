ed6bb209eecb9a65d2680e8570a5ec628437acd8
cdms <cmds@example.com>
2022-03-31 09:27:40 -0500

    Got overnight run of data on ANSK_Custom firmware.

--- test_firmwares/ANSK_Custom/library/Python/plot.py
+++ test_firmwares/ANSK_Custom/library/Python/plot.py
@@ -10,9 +10,9 @@ matplotlib.use('tkagg') #Choose backend that can be x-forwarded to windows
 #Get and arrange data
 sourcedata = pd.read_csv("../../data/out.csv",header=None,usecols=[0]).to_numpy().flatten()
 #backgrounddata = pd.read_csv("../../data/out.csv",header=None,usecols=[0]).to_numpy().flatten()
-sourcedata = np.subtract(sourcedata,200*8192) 	#subtract the baseline (8192) times the gate value.
+#sourcedata = np.subtract(sourcedata,200*8192) 	#subtract the baseline (8192) times the gate value.
 #backgrounddata = np.subtract(backgrounddata,200*8192)
-min = sourcedata.min()
+min = 1000 #sourcedata.min()
 max = sourcedata.max()
 print("min: ",min,", max: ",max)
 print(sourcedata)
