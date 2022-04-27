1e2f2124141afa624f1e0ad8d48dfb23a1729aa0
nuclearGoblin <kathryn.harris@ucdenver.edu>
2022-04-27 12:47:09 -0600

    Create programs to scan window trigger at higher upper thresholds.

--- test_firmwares/UniversalTrigger1/library/C/lib/compile.sh
+++ test_firmwares/UniversalTrigger1/library/C/lib/compile.sh
@@ -2,3 +2,4 @@
 gcc -o scanrate.exe src/scanrate.c -L. ./gcc/libUniversalTrigger1.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
 gcc -o scanwindow.exe src/scanwindow.c -L. ./gcc/libUniversalTrigger1.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
 gcc -o setthresh.exe src/setthresh.c -L. ./gcc/libUniversalTrigger1.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
+gcc -o scanwindow_high.exe src/scanwindow_high.c -L. ./gcc/libUniversalTrigger1.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
