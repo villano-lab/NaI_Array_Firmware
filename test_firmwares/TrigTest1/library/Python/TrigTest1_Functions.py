39d011c8464981e5fe8ee2fd8b526a5b818d86ee
villaa <villaa@gmail.com>
2022-05-07 20:25:02 -0600

    [ANV] added a TrigTest1 with a leading edge trigger

---
+++ test_firmwares/TrigTest1/library/Python/TrigTest1_Functions.py
@@ -0,0 +1,205 @@
+
+
+
+import TrigTest1_RegisterFile
+from ctypes import *
+import array
+import numpy as np
+import os
+
+mydll = cdll.LoadLibrary(os.path.dirname(__file__) +'/SCIDK_Lib.dll')
+
+def ConnectDevice(board):
+    handle = c_void_p(256)
+    err = mydll.SCIDK_ConnectUSB(board, byref(handle))
+    return err, handle
+
+def CloseConnect(handle):
+    err = mydll.NI_CloseConnection(byref(handle))
+    return err	
+	
+def ListDevices():
+    count = c_int(0)
+    s = create_string_buffer(2048)
+    err = mydll.NI_USBEnumerate(byref(s), b"SCIDK", byref(count))
+    str_con = (s.value.decode('ascii')) 
+    str_devices = str_con.split(';')
+    dev_count = count.value
+    return str_devices, dev_count 
+
+def __abstracted_reg_write(data, address, handle):
+    err = mydll.NI_WriteReg(data, address, byref(handle))
+    return err
+
+def __abstracted_reg_read(address, handle):
+    data = c_uint(0)
+    err = mydll.NI_ReadReg(byref(data), address, byref(handle))
+    return err, data.value
+
+def __abstracted_mem_write(data, count, address, timeout_ms, handle):
+    written_data = c_uint(0)
+    err = mydll.NI_WriteData(data, count, address, 0, timeout_ms, byref(handle), byref(written_data))
+    return err, written_data.value
+
+def __abstracted_mem_read(count, address, timeout_ms, handle):
+    data = (c_uint * (2* count))()
+    read_data = c_uint(0)
+    valid_data = c_uint(0)
+    err = mydll.NI_ReadData(byref(data), count, address, 0, timeout_ms, byref(handle), byref(read_data), byref(valid_data))
+    return err, data, read_data.value, valid_data.value
+
+def __abstracted_fifo_write(data, count, address, timeout_ms, handle):
+    written_data = c_uint(0)
+    err = mydll.NI_WriteData(data, count, address, 1, timeout_ms, byref(handle), byref(written_data))
+    return err, written_data.value
+
+def __abstracted_fifo_read(count, address, address_status, blocking, timeout_ms, handle):
+    data = (c_uint * (2 * count))()
+    read_data = c_uint(0)
+    valid_data = c_uint(0)
+    err = mydll.NI_ReadData(byref(data), count, address, 1, timeout_ms, byref(handle), byref(read_data), byref(valid_data))
+    return err, data, read_data, valid_data 	
+	
+
+def REG_pol_GET(handle):
+    [err, data] = __abstracted_reg_read(TrigTest1_RegisterFile.SCI_REG_pol, handle)
+    return err, data
+
+def REG_pol_SET(data, handle):
+    err = __abstracted_reg_write(data, TrigTest1_RegisterFile.SCI_REG_pol, handle)
+    return err
+
+def REG_inhib_GET(handle):
+    [err, data] = __abstracted_reg_read(TrigTest1_RegisterFile.SCI_REG_inhib, handle)
+    return err, data
+
+def REG_inhib_SET(data, handle):
+    err = __abstracted_reg_write(data, TrigTest1_RegisterFile.SCI_REG_inhib, handle)
+    return err
+
+def REG_thrs_GET(handle):
+    [err, data] = __abstracted_reg_read(TrigTest1_RegisterFile.SCI_REG_thrs, handle)
+    return err, data
+
+def REG_thrs_SET(data, handle):
+    err = __abstracted_reg_write(data, TrigTest1_RegisterFile.SCI_REG_thrs, handle)
+    return err
+
+def REG_trigpol_GET(handle):
+    [err, data] = __abstracted_reg_read(TrigTest1_RegisterFile.SCI_REG_trigpol, handle)
+    return err, data
+
+def REG_trigpol_SET(data, handle):
+    err = __abstracted_reg_write(data, TrigTest1_RegisterFile.SCI_REG_trigpol, handle)
+    return err
+def REG_ANALOG_OFFSET_SET(data, handle):
+    err = __abstracted_reg_write(data, TrigTest1_RegisterFile.SCI_REG_ANALOG_OFFSET, handle)
+    return err
+
+
+
+def OSCILLOSCOPE_Oscilloscope_0_START(handle):
+    err = __abstracted_reg_write(0, TrigTest1_RegisterFile.SCI_REG_Oscilloscope_0_CONFIG_ARM, handle)
+    if (err != 0):
+       return False
+    err = __abstracted_reg_write(1, TrigTest1_RegisterFile.SCI_REG_Oscilloscope_0_CONFIG_ARM, handle)
+    if (err != 0):
+       return False
+    return True
+
+def OSCILLOSCOPE_Oscilloscope_0_SET_DECIMATOR(OscilloscopeDecimator, handle):
+    err = __abstracted_reg_write(OscilloscopeDecimator, TrigTest1_RegisterFile.SCI_REG_Oscilloscope_0_CONFIG_DECIMATOR, handle)
+    return err
+
+def OSCILLOSCOPE_Oscilloscope_0_SET_PRETRIGGER(OscilloscopePreTrigger, handle):
+    err = __abstracted_reg_write(OscilloscopePreTrigger, TrigTest1_RegisterFile.SCI_REG_Oscilloscope_0_CONFIG_PRETRIGGER, handle)
+    return err
+
+def OSCILLOSCOPE_Oscilloscope_0_SET_TRIGGER_LEVEL(OscilloscopeTriggerLevel, handle):
+    err = __abstracted_reg_write(OscilloscopeTriggerLevel, TrigTest1_RegisterFile.SCI_REG_Oscilloscope_0_CONFIG_TRIGGER_LEVEL, handle)
+    return err
+
+def OSCILLOSCOPE_Oscilloscope_0_SET_TRIGGER_MODE(OscilloscopeTriggerMode, OscilloscopeTriggerChannel, OscilloscopeTriggerEdge, handle):
+    AnalogTrigger = 0
+    Digital0Trigger = 0
+    Digital1Trigger = 0
+    Digital2Trigger = 0
+    Digital3Trigger = 0
+    SoftwareTrigger = 0
+    if (OscilloscopeTriggerMode == "Analog"):
+        AnalogTrigger = 1
+    if (OscilloscopeTriggerMode == "Digital0"):
+        Digital0Trigger = 1
+    if (OscilloscopeTriggerMode == "Digital1"):
+        Digital1Trigger = 1
+    if (OscilloscopeTriggerMode == "Digital2"):
+        Digital2Trigger = 1
+    if (OscilloscopeTriggerMode == "Digital3"):
+        Digital3Trigger = 1
+    if (OscilloscopeTriggerMode == "Free"):
+        SoftwareTrigger = 1
+    if (OscilloscopeTriggerEdge == "Rising"):
+        Edge = 0
+    else:
+        Edge = 1
+    triggermode = c_int(0)
+    triggermode = (OscilloscopeTriggerChannel << 8)  + (SoftwareTrigger << 7 ) + (Edge << 3) + (SoftwareTrigger << 1) + AnalogTrigger +(Digital0Trigger << 2) + (Digital1Trigger << 2) + Digital1Trigger + (Digital2Trigger << 2) + (Digital2Trigger << 1) + (Digital3Trigger << 2) + (Digital3Trigger << 1) + Digital3Trigger
+    err = __abstracted_reg_write(triggermode, TrigTest1_RegisterFile.SCI_REG_Oscilloscope_0_CONFIG_TRIGGER_MODE, handle)
+    return err
+
+def OSCILLOSCOPE_Oscilloscope_0_GET_STATUS(handle):
+    [err, status] = __abstracted_reg_read(TrigTest1_RegisterFile.SCI_REG_Oscilloscope_0_READ_STATUS, handle)
+    return err, status
+
+def OSCILLOSCOPE_Oscilloscope_0_GET_POSITION(handle):
+    [err, position] = __abstracted_reg_read(TrigTest1_RegisterFile.SCI_REG_Oscilloscope_0_READ_POSITION, handle)
+    return err, position
+
+def OSCILLOSCOPE_Oscilloscope_0_GET_DATA(timeout_ms, handle):
+    [err, data, read_data, valid_data] = __abstracted_mem_read(1024, TrigTest1_RegisterFile.SCI_REG_Oscilloscope_0_FIFOADDRESS, timeout_ms, handle)
+    return err, data, read_data, valid_data
+
+def OSCILLOSCOPE_Oscilloscope_0_RECONSTRUCT_DATA(OscilloscopeData, OscilloscopePosition, OscilloscopePreTrigger):
+    OscilloscopeChannels = 1
+    OscilloscopeSamples = 1024
+    Analog = list(range(OscilloscopeSamples*OscilloscopeChannels))
+    Digital0 = list(range(OscilloscopeSamples*OscilloscopeChannels))
+    Digital1 = list(range(OscilloscopeSamples*OscilloscopeChannels))
+    Digital2 = list(range(OscilloscopeSamples*OscilloscopeChannels))
+    Digital3 = list(range(OscilloscopeSamples*OscilloscopeChannels))
+    for n in range(OscilloscopeChannels):
+        current = OscilloscopePosition - OscilloscopePreTrigger
+        if ((current) > 0):
+            k = 0
+            for i in range(current, OscilloscopeSamples-1):
+                Analog[k+ OscilloscopeSamples * n] = OscilloscopeData[i+ OscilloscopeSamples * n] & 65535
+                Digital0[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 16 & 1)
+                Digital1[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 17 & 1)
+                Digital2[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 18 & 1)
+                Digital3[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 19 & 1)
+                k = k + 1
+            for i in range(0, current-1):
+                Analog[k+ OscilloscopeSamples * n] = OscilloscopeData[i+ OscilloscopeSamples * n] & 65535
+                Digital0[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 16 & 1)
+                Digital1[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 17 & 1)
+                Digital2[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 18 & 1)
+                Digital3[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 19 & 1)
+                k = k + 1
+        else:
+            k = 0
+            for i in range(OscilloscopeSamples+current, OscilloscopeSamples-1):
+                Analog[k+ OscilloscopeSamples * n] = OscilloscopeData[i+ OscilloscopeSamples * n] & 65535
+                Digital0[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 16 & 1)
+                Digital1[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 17 & 1)
+                Digital2[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 18 & 1)
+                Digital3[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 19 & 1)
+                k = k + 1
+            for i in range(0, OscilloscopeSamples+current-1):
+                Analog[k+ OscilloscopeSamples * n] = OscilloscopeData[i+ OscilloscopeSamples * n] & 65535
+                Digital0[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 16 & 1)
+                Digital1[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 17 & 1)
+                Digital2[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 18 & 1)
+                Digital3[k+ OscilloscopeSamples * n] = (OscilloscopeData[i+ OscilloscopeSamples * n] >> 19 & 1)
+                k = k + 1
+    return Analog, Digital0, Digital1,Digital2, Digital3
+
---
+++ test_firmwares/TrigTest1/output/TrigTest1/TrigTest1.srcs/sources_1/ip/main_clk_gen/main_clk_gen_stub.vhdl
@@ -0,0 +1,31 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+-- Date        : Sat May  7 20:17:25 2022
+-- Host        : PHYS-NC3124-D02 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               C:/OpenHardware/UserProject/NaI_Array_Firmware/test_firmwares/TrigTest1/output/TrigTest1/TrigTest1.srcs/sources_1/ip/main_clk_gen/main_clk_gen_stub.vhdl
+-- Design      : main_clk_gen
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7s25csga225-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity main_clk_gen is
+  Port ( 
+    sample_clk : out STD_LOGIC;
+    clk_out2 : out STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end main_clk_gen;
+
+architecture stub of main_clk_gen is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "sample_clk,clk_out2,locked,clk_in1";
+begin
+end;
