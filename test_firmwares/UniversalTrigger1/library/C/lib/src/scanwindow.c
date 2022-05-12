fcedd43a49396db6aee4585002ad81eb6be090d6
Anthony Villano <villaa@gmail.com>
2022-05-12 14:25:22 -0600

    [ANV] update delay and gate on scanwindow

--- test_firmwares/UniversalTrigger1/library/C/lib/src/scanwindow.c
+++ test_firmwares/UniversalTrigger1/library/C/lib/src/scanwindow.c
@@ -266,8 +266,8 @@ int main(int argc, char* argv[])
 	//Configure settings
         int top = thrs; //top of the window in trigger window
 	int inhib = 50;		//inhibition time on trigger block
-        int delay=3;
-        int gate=10;
+        int delay=50;
+        int gate=100;
 	//things you probably won't change
 	int polarity = 0;	//zero for negative, one for positive
 	//things that are set based on external factors
