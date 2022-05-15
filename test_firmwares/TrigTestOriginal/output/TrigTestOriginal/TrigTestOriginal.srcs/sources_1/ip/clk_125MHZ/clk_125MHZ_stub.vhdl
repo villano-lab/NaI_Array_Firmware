7bfb7cc046c47aed50821b8bdee4c10dee5e67f9
villaa <villaa@gmail.com>
2022-05-15 17:48:51 -0600

    [ANV] put original firmware in too just as an example, inversion has 16 effective bits which is wrong

---
+++ test_firmwares/TrigTestOriginal/output/TrigTestOriginal/TrigTestOriginal.srcs/sources_1/ip/clk_125MHZ/clk_125MHZ_stub.vhdl
@@ -0,0 +1,29 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+-- Date        : Sun May 15 16:49:07 2022
+-- Host        : PHYS-NC3124-D02 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               C:/OpenHardware/UserProject/NaI_Array_Firmware/test_firmwares/TrigTestOriginal/output/TrigTestOriginal/TrigTestOriginal.srcs/sources_1/ip/clk_125MHZ/clk_125MHZ_stub.vhdl
+-- Design      : clk_125MHZ
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7s25csga225-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_125MHZ is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end clk_125MHZ;
+
+architecture stub of clk_125MHZ is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,clk_in1";
+begin
+end;
