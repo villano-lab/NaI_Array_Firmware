c466f3866f358805e30cd18736261fdc5590f8ca
Kitty <kathryn.harris@ucdenver.edu>
2022-05-16 14:42:32 -0600

    Create software for UT1A

---
+++ test_firmwares/UniversalTrigger1A/library/C/lib/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+gcc -o scanrate.exe src/scanrate.c -L. ./gcc/libUniversalTrigger1A.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
+gcc -o scanwindow.exe src/scanwindow.c -L. ./gcc/libUniversalTrigger1A.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
+gcc -o setthresh.exe src/setthresh.c -L. ./gcc/libUniversalTrigger1A.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
+gcc -o scanwindow_high.exe src/scanwindow_high.c -L. ./gcc/libUniversalTrigger1A.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
