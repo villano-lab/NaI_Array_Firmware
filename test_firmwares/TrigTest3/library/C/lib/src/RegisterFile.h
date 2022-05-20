3c4e4ecce23982ded5e004f33519cc4345599105
villaa <villaa@gmail.com>
2022-05-19 19:41:50 -0600

    [ANV] adding a TrigTest3 to get a spectrum and put trigger in sub-diagram

---
+++ test_firmwares/TrigTest3/library/C/lib/src/RegisterFile.h
@@ -0,0 +1,19 @@
+#define SCI_REG_ALL_FIFO_RESET 0xFFFFF908
+#define SCI_REG_pol 0x00000000
+#define SCI_REG_thrs 0x00000001
+#define SCI_REG_inhib 0x00000002
+#define SCI_REG_top 0x00000003
+#define SCI_REG_delay 0x00000004
+#define SCI_REG_gate 0x00000005
+#define SCI_REG_ANALOG_OFFSET 0xFFFFFFF9
+#define SCI_REG_Oscilloscope_0_FIFOADDRESS 0x400
+#define SCI_REG_Oscilloscope_0_READ_STATUS 0x800
+#define SCI_REG_Oscilloscope_0_READ_POSITION 0x801
+#define SCI_REG_Oscilloscope_0_CONFIG_TRIGGER_MODE 0x802
+#define SCI_REG_Oscilloscope_0_CONFIG_PRETRIGGER 0x803
+#define SCI_REG_Oscilloscope_0_CONFIG_TRIGGER_LEVEL 0x804
+#define SCI_REG_Oscilloscope_0_CONFIG_ARM 0x805
+#define SCI_REG_Oscilloscope_0_CONFIG_DECIMATOR 0x806
+
+#define SCI_REG_RateMeter_0_FIFOADDRESS 0x10000
+
