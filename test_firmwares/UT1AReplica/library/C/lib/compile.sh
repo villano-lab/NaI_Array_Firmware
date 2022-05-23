8338728ded535208ca3f869eea20ed5f084dde6e
Kitty <kathryn.harris@ucdenver.edu>
2022-05-23 17:40:56 -0600

    got program compiling!

--- test_firmwares/UT1AReplica/library/C/lib/compile.sh
+++ test_firmwares/UT1AReplica/library/C/lib/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -o scanwindow.exe src/scanwindow.c src/Ut1AReplica_lib.c src/circular_buffer.c -L ../../../../SCIDK-SDKLinux/libSCIDK/Debug/libSCIDK.so
+gcc -o scanwindow.exe src/scanwindow.c src/Ut1AReplica_lib.c src/circular_buffer.c -L. ../../../../SCIDK-SDKLinux/libSCIDK/Debug/libSCIDK.so
