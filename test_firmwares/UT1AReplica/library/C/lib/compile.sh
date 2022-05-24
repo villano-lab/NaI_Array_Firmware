7078dbf84141b69bc3c0b51aa7266cac0f67913d
Anthony Villano <villaa@gmail.com>
2022-05-24 12:08:13 -0600

    [ANV] updated example program

--- test_firmwares/UT1AReplica/library/C/lib/compile.sh
+++ test_firmwares/UT1AReplica/library/C/lib/compile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 gcc -o scanwindow.exe src/scanwindow.c src/UT1AReplica_lib.c src/circular_buffer.c -L. ../../../../SCIDK-SDKLinux/libSCIDK/Debug/libSCIDK.so
+gcc -o test.exe src/UT1AReplica_example.c src/UT1AReplica_lib.c src/circular_buffer.c -L. ../../../../SCIDK-SDKLinux/libSCIDK/Debug/libSCIDK.so
