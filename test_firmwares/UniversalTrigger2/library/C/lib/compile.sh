318c26673d99e85e557756280fd2448422270a6a
nuclearGoblin <kathryn.harris@ucdenver.edu>
2022-05-27 16:59:01 -0600

    Copy software over in preparation for work on UT2 which will be a fix for the polarity inversion bug

--- test_firmwares/UniversalTrigger2/library/C/lib/compile.sh
+++ test_firmwares/UniversalTrigger2/library/C/lib/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -o scanrate.exe src/scanrate.c -L. ./gcc/libUniversalTrigger2.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
-gcc -o scanwindow.exe src/scanwindow.c -L. ./gcc/libUniversalTrigger2.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
-gcc -o setthresh.exe src/setthresh.c -L. ./gcc/libUniversalTrigger2.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
+gcc -o scanrate.exe src/scanrate.c src/UniversalTriggerShared.c -L. ./gcc/libUniversalTrigger2.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
+gcc -o scanwindow.exe src/scanwindow.c src/UniversalTriggerShared.c -L. ./gcc/libUniversalTrigger2.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
+gcc -o setthresh.exe src/setthresh.c src/UniversalTriggerShared.c -L. ./gcc/libUniversalTrigger2.so ../../../../r5560_sdk/linux/libr5560.so.1.0.0
