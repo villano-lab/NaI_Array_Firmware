7a1476c9d1a1f3ae9199d630ba19ce25f26e6cb1
cdms <vlab.ucdenver@gmail.com>
2022-06-01 18:02:03 -0500

    [AK] Add UT3

---
+++ test_firmwares/compile_sw/library/Python/compile_sw_example.py
@@ -0,0 +1,23 @@
+from compile_sw_Functions import *
+from ctypes import *
+import matplotlib.pyplot as plt
+import time
+
+[ListOfDevices, count] = ListDevices()
+if (count > 0):
+
+    board = ListOfDevices[0].encode('utf-8')
+
+    [err, handle] = ConnectDevice(board)
+    if (err == 0):
+        print("Successful connection to board ", board)
+    else:
+        print("Connection Error")
+        exit(-1)
+    
+    while True:
+        r1 = REG_AN_IN_GET(handle)[1]
+        r2 = REG_CNTR_GET(handle)[1]
+        print("Analog value: " + str(r1) + "   Timer:" + str(r2) + "\n")
+
+
