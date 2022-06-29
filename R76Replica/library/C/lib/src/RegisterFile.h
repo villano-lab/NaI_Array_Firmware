4bedd44376ac6c8474a90b4d2024d3297a86f305
villaa <kathryn.harris@ucdenver.edu>
2022-06-29 13:41:12 -0600

    Create replica of R76 firmware for SCIDK
    Using this to troubleshoot issues with rate reduction

---
+++ R76Replica/library/C/lib/src/RegisterFile.h
@@ -0,0 +1,29 @@
+#define SCI_REG_ALL_FIFO_RESET 0xFFFFF908
+#define SCI_REG_gate_l 0x00000000
+#define SCI_REG_polarity 0x00000001
+#define SCI_REG_thrsh 0x00000002
+#define SCI_REG_inhib 0x00000003
+#define SCI_REG_delay 0x00000004
+#define SCI_REG_ratereset 0x00000005
+#define SCI_REG_gate_u 0x00000006
+#define SCI_REG_top 0x00000007
+#define SCI_REG_disable_A 0x00000008
+#define SCI_REG_disable_B 0x00000009
+#define SCI_REG_skip 0x0000000A
+#define SCI_REG_count 0x00040000
+#define SCI_REG_skip_attempts 0x00041007
+#define SCI_REG_eq_count 0x00041008
+#define SCI_REG_ANALOG_OFFSET 0xFFFFFFF9
+#define SCI_REG_RateMeter_0_FIFOADDRESS 0x30000
+
+#define SCI_REG_RateMeter_Reduced_FIFOADDRESS 0x10000
+
+#define SCI_REG_Oscilloscope_0_FIFOADDRESS 0x40800
+#define SCI_REG_Oscilloscope_0_READ_STATUS 0x41000
+#define SCI_REG_Oscilloscope_0_READ_POSITION 0x41001
+#define SCI_REG_Oscilloscope_0_CONFIG_TRIGGER_MODE 0x41002
+#define SCI_REG_Oscilloscope_0_CONFIG_PRETRIGGER 0x41003
+#define SCI_REG_Oscilloscope_0_CONFIG_TRIGGER_LEVEL 0x41004
+#define SCI_REG_Oscilloscope_0_CONFIG_ARM 0x41005
+#define SCI_REG_Oscilloscope_0_CONFIG_DECIMATOR 0x41006
+
