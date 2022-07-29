ef2a74109d521db65e24c18223684a085b18eb8a
villaa <villaa@gmail.com>
2022-07-29 15:09:26 -0600

    [ANV] version of ANSK for shaped/amplified exponential signals

---
+++ test_firmwares/ANSK029amp/library/C/lib/src/RegisterFile.h
@@ -0,0 +1,32 @@
+#define SCI_REG_ALL_FIFO_RESET 0xFFFFF908
+#define SCI_REG_pol 0x00000000
+#define SCI_REG_ofs 0x00000001
+#define SCI_REG_thrs 0x00000002
+#define SCI_REG_TK 0x00000003
+#define SCI_REG_TM 0x00000004
+#define SCI_REG_K 0x00000005
+#define SCI_REG_decovM 0x00000006
+#define SCI_REG_M 0x00000007
+#define SCI_REG_gain 0x00000008
+#define SCI_REG_blen 0x00000009
+#define SCI_REG_blinib 0x0000000A
+#define SCI_REG_samplepos 0x0000000B
+#define SCI_REG_runcfg 0x0000000C
+#define SCI_REG_ANALOG_OFFSET 0xFFFFFFF9
+#define SCI_REG_Oscilloscope_0_FIFOADDRESS 0x2000
+#define SCI_REG_Oscilloscope_0_READ_STATUS 0x4000
+#define SCI_REG_Oscilloscope_0_READ_POSITION 0x4001
+#define SCI_REG_Oscilloscope_0_CONFIG_TRIGGER_MODE 0x4002
+#define SCI_REG_Oscilloscope_0_CONFIG_PRETRIGGER 0x4003
+#define SCI_REG_Oscilloscope_0_CONFIG_TRIGGER_LEVEL 0x4004
+#define SCI_REG_Oscilloscope_0_CONFIG_ARM 0x4005
+#define SCI_REG_Oscilloscope_0_CONFIG_DECIMATOR 0x4006
+
+#define SCI_REG_Spectrum_0_FIFOADDRESS 0x10000
+#define SCI_REG_Spectrum_0_STATUS 0x20000
+#define SCI_REG_Spectrum_0_CONFIG 0x20001
+#define SCI_REG_Spectrum_0_CONFIG_LIMIT 0x20002
+#define SCI_REG_Spectrum_0_CONFIG_REBIN 0x20003
+#define SCI_REG_Spectrum_0_CONFIG_MIN 0x20004
+#define SCI_REG_Spectrum_0_CONFIG_MAX 0x20005
+
