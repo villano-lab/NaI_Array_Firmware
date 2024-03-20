f5ac4caf7fd3b44accb75f0035ca96c5ff41692c
villaa <kathryn.harris@ucdenver.edu>
2024-03-20 12:18:38 -0600

    Add vhdlclock copy for scikit
    Mostly working, having clock rate issues due to available clock rates

---
+++ test_firmwares/vhdlclock_scikit/library/Legacy/C/lib/src/Def.h
@@ -0,0 +1,21 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <stdint.h>
+#pragma once
+
+
+typedef enum 
+{
+	NOT_CONNECTED = 0,
+	CONNECTED = 1
+}USB_CONNECTION_STATUS ;
+
+
+typedef enum 
+{
+	REG_ACCESS = 0,
+	STREAMING = 1
+} USB_BUS_MODE;
+
+
+
