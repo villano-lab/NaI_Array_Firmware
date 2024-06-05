173223228db899aefe14662dbe68b7680243692b
villaa <kathryn.harris@ucdenver.edu>
2024-06-05 11:32:37 -0600

    Get version with FIFO properly compiled
    Was missing RegisterFile updates previously.
    Went ahead and increased the FIFO size as well, can't hurt.

--- R76Replica/library/Legacy/C/lib/src/RegisterFile.h
+++ R76Replica/library/Legacy/C/lib/src/RegisterFile.h
@@ -1,24 +1,22 @@
 #define SCI_REG_ALL_FIFO_RESET 0xFFFFF908
-#define SCI_REG_unequal 0x0006200B
-#define SCI_REG_tc_nonzero 0x0006200C
-#define SCI_REG_trigger_code 0x0006200D
-#define SCI_REG_timestamp 0x0006200E
-#define SCI_REG_reset 0x0006200F
-#define SCI_REG_timestamp2 0x00062010
+#define SCI_REG_unequal 0x0004200B
+#define SCI_REG_tc_nonzero 0x0004200C
+#define SCI_REG_trigger_code 0x0004200D
+#define SCI_REG_timestamp 0x0004200E
+#define SCI_REG_reset 0x0004200F
+#define SCI_REG_timestamp2 0x00042010
 #define SCI_REG_ANALOG_OFFSET 0xFFFFFFF9
-#define SCI_REG_disable_det_CH0 0x60010
-#define SCI_REG_disable_det_CH1 0x60011
+#define SCI_REG_disable_det_CH0 0x40010
+#define SCI_REG_disable_det_CH1 0x40011
 
-#define SCI_REG_thrsh_CH0 0x60008
-#define SCI_REG_thrsh_CH1 0x60009
+#define SCI_REG_thrsh_CH0 0x40008
+#define SCI_REG_thrsh_CH1 0x40009
 
-#define SCI_REG_top_CH0 0x62009
-#define SCI_REG_top_CH1 0x6200A
+#define SCI_REG_top_CH0 0x42009
+#define SCI_REG_top_CH1 0x4200A
 
-#define SCI_REG_base_CH0 0x60014
-#define SCI_REG_base_CH1 0x60015
-
-#define SCI_REG_RateMeter_0_FIFOADDRESS 0x30000
+#define SCI_REG_base_CH0 0x40014
+#define SCI_REG_base_CH1 0x40015
 
 #define SCI_REG_trig_polarity 0x1
 #define SCI_REG_trig_inhib 0x2
@@ -26,8 +24,8 @@
 #define SCI_REG_trig_gate_u 0x4
 #define SCI_REG_trig_delay 0x5
 
-#define SCI_REG_int_time 0x6000C
-#define SCI_REG_int_pre 0x6000D
+#define SCI_REG_int_time 0x4000C
+#define SCI_REG_int_pre 0x4000D
 
 #define SCI_REG_Spectrum_0_FIFOADDRESS 0x10000
 #define SCI_REG_Spectrum_0_STATUS 0x20000
@@ -37,38 +35,37 @@
 #define SCI_REG_Spectrum_0_CONFIG_MIN 0x20004
 #define SCI_REG_Spectrum_0_CONFIG_MAX 0x20005
 
-#define SCI_REG_Spectrum_1_FIFOADDRESS 0x50000
-#define SCI_REG_Spectrum_1_STATUS 0x60000
-#define SCI_REG_Spectrum_1_CONFIG 0x60001
-#define SCI_REG_Spectrum_1_CONFIG_LIMIT 0x60002
-#define SCI_REG_Spectrum_1_CONFIG_REBIN 0x60003
-#define SCI_REG_Spectrum_1_CONFIG_MIN 0x60004
-#define SCI_REG_Spectrum_1_CONFIG_MAX 0x60005
+#define SCI_REG_Spectrum_1_FIFOADDRESS 0x30000
+#define SCI_REG_Spectrum_1_STATUS 0x40000
+#define SCI_REG_Spectrum_1_CONFIG 0x40001
+#define SCI_REG_Spectrum_1_CONFIG_LIMIT 0x40002
+#define SCI_REG_Spectrum_1_CONFIG_REBIN 0x40003
+#define SCI_REG_Spectrum_1_CONFIG_MIN 0x40004
+#define SCI_REG_Spectrum_1_CONFIG_MAX 0x40005
 
-#define SCI_REG_Oscilloscope_1_FIFOADDRESS 0x61000
-#define SCI_REG_Oscilloscope_1_READ_STATUS 0x62000
-#define SCI_REG_Oscilloscope_1_READ_POSITION 0x62001
-#define SCI_REG_Oscilloscope_1_CONFIG_TRIGGER_MODE 0x62002
-#define SCI_REG_Oscilloscope_1_CONFIG_PRETRIGGER 0x62003
-#define SCI_REG_Oscilloscope_1_CONFIG_TRIGGER_LEVEL 0x62004
-#define SCI_REG_Oscilloscope_1_CONFIG_ARM 0x62005
-#define SCI_REG_Oscilloscope_1_CONFIG_DECIMATOR 0x62006
+#define SCI_REG_Oscilloscope_1_FIFOADDRESS 0x41000
+#define SCI_REG_Oscilloscope_1_READ_STATUS 0x42000
+#define SCI_REG_Oscilloscope_1_READ_POSITION 0x42001
+#define SCI_REG_Oscilloscope_1_CONFIG_TRIGGER_MODE 0x42002
+#define SCI_REG_Oscilloscope_1_CONFIG_PRETRIGGER 0x42003
+#define SCI_REG_Oscilloscope_1_CONFIG_TRIGGER_LEVEL 0x42004
+#define SCI_REG_Oscilloscope_1_CONFIG_ARM 0x42005
+#define SCI_REG_Oscilloscope_1_CONFIG_DECIMATOR 0x42006
 
-#define SCI_REG_diag_time_lower 0x62013
-#define SCI_REG_diag_time_upper 0x62014
-#define SCI_REG_diag_time_full 0x62015
-#define SCI_REG_diag_time_normal 0x62016
+#define SCI_REG_diag_time_lower 0x42013
+#define SCI_REG_diag_time_upper 0x42014
+#define SCI_REG_diag_time_full 0x42015
+#define SCI_REG_diag_time_normal 0x42016
 
-#define SCI_REG_diag_greater 0x62019
-#define SCI_REG_diag_FM_Out 0x6201A
-#define SCI_REG_diag_to_encode 0x6201B
-#define SCI_REG_diag_timing 0x6201C
-#define SCI_REG_diag_full 0x6201D
-#define SCI_REG_diag_counts 0x6201E
-#define SCI_REG_diag_debug 0x6201F
+#define SCI_REG_All_Energies_FIFOADDRESS 0x42018
+#define SCI_REG_All_Energies_READ_STATUS 0x42019
+#define SCI_REG_All_Energies_READ_VALID_WORDS 0x4201A
+#define SCI_REG_All_Energies_CONFIG 0x4201B
 
-#define SCI_REG_All_Energies_FIFOADDRESS 0x62021
-#define SCI_REG_All_Energies_READ_STATUS 0x62022
-#define SCI_REG_All_Energies_READ_VALID_WORDS 0x62023
-#define SCI_REG_All_Energies_CONFIG 0x62024
+#define SCI_REG_RateMeter_0_FIFOADDRESS 0x50000
+
+#define SCI_REG_diag_FM_Out 0x60002
+#define SCI_REG_diag_to_encode 0x60003
+#define SCI_REG_diag_full 0x60004
+#define SCI_REG_diag_debug 0x60005
 
