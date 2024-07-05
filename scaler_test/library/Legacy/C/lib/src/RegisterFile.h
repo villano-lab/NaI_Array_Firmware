86ee5e76e49c7face77350e2a63296eb651ca83d
Johnny <garbage@gmail.com>
2024-07-05 10:20:27 -0500

    Create a test of scaler block and LEMO outputs

---
+++ scaler_test/library/Legacy/C/lib/src/RegisterFile.h
@@ -0,0 +1,10 @@
+#define SCI_REG_divide 0x00000000
+#define SCI_REG_Oscilloscope_0_FIFOADDRESS 0x400
+#define SCI_REG_Oscilloscope_0_READ_STATUS 0x800
+#define SCI_REG_Oscilloscope_0_READ_POSITION 0x801
+#define SCI_REG_Oscilloscope_0_CONFIG_TRIGGER_MODE 0x802
+#define SCI_REG_Oscilloscope_0_CONFIG_PRETRIGGER 0x803
+#define SCI_REG_Oscilloscope_0_CONFIG_TRIGGER_LEVEL 0x804
+#define SCI_REG_Oscilloscope_0_CONFIG_ARM 0x805
+#define SCI_REG_Oscilloscope_0_CONFIG_DECIMATOR 0x806
+
