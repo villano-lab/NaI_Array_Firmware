f909efbbef249c6def378009ba8ff06f7f3af3d2
Johnny <garbage@gmail.com>
2024-07-05 15:01:15 -0500

    create test of lemo outputs
    something weird happening between 1 and 2

---
+++ lemo_out_test/library/Legacy/C/lib/src/RegisterFile.h
@@ -0,0 +1,15 @@
+#define SCI_REG_divide 0x00000000
+#define SCI_REG_io_to_encode 0x3
+#define SCI_REG_io_stamptype 0x4
+#define SCI_REG_io_lemo2out 0x5
+#define SCI_REG_io_divide 0x6
+
+#define SCI_REG_diag_FIFOADDRESS 0x400
+#define SCI_REG_diag_READ_STATUS 0x800
+#define SCI_REG_diag_READ_POSITION 0x801
+#define SCI_REG_diag_CONFIG_TRIGGER_MODE 0x802
+#define SCI_REG_diag_CONFIG_PRETRIGGER 0x803
+#define SCI_REG_diag_CONFIG_TRIGGER_LEVEL 0x804
+#define SCI_REG_diag_CONFIG_ARM 0x805
+#define SCI_REG_diag_CONFIG_DECIMATOR 0x806
+
